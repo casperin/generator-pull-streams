ee652aaa38efca106ba1ab4dcaaec32d84ff5fe2
Gorm Casper <g@g.dk>
2016-10-27 17:44:18 +0000

    add util/index

---
+++ util/index.js
@@ -0,0 +1,14 @@
+module.exports ={
+  filter: require('./filter'),
+  find: require('./find'),
+  flatten: require('./flatten'),
+  map: require('./map'),
+  notUnique: require('./notUnique'),
+  reject: require('./reject'),
+  scan: require('./scan'),
+  take: require('./take'),
+  tap: require('./tap'),
+  throttle: require('./throttle'),
+  unique: require('./unique'),
+  until: require('./until')
+}
